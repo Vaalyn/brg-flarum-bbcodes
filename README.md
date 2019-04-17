277b32ecf7316ae9692b5351ab7b1e4bf8d40580
Vaalyn <vaalyndev@gmail.com>
2019-04-17 23:11:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Diese Erweiterung für Flarum fügt einige BBCodes hinzu die für das Forum des Brony Radio Germany gebraucht werden.
+
+## Installation
+
+`composer require vaalyn/brg-flarum-bbcodes`
+
+## Updaten
+
+1) `composer update vaalyn/brg-flarum-bbcodes`
+
+2) Aktiviere / Deaktiviere die Erweiterung in der Administration des Forums
+
+3) Leere den Cache des Forums
+
+## Deinstallieren
+
+`composer remove vaalyn/brg-flarum-bbcodes`
+
+## Benutzen
+
+`[hr]` fügt eine <hr/> ein
---
+++ extend.php
@@ -0,0 +1,20 @@
+<?php
+
+use Flarum\Extend;
+use Flarum\Frontend\Document;
+use s9e\TextFormatter\Configurator;
+
+return [
+    (new Extend\Frontend('forum'))
+        ->content(function (Document $document) {
+            $document->head[] = '<link rel="stylesheet" type="text/css" href="/assets/extensions/brg-flarum-bbcodes/styles.css">';
+        }),
+
+    (new Extend\Formatter)
+        ->configure(function (Configurator $config) {
+            $config->BBCodes->addCustom(
+                '[hr]',
+                '<hr/>'
+            );
+		})
+];
